f1b4914156b3df355a93ce3472392ef933f38647
shoogen <73682940+shoogen@users.noreply.github.com>
2025-07-21 04:08:28 -0400

    - Hopefully improving dynamic load of regional data

--- Setup.lua
+++ Setup.lua
@@ -5,17 +5,11 @@ local frame = CreateFrame("Frame");
 frame:RegisterEvent("PLAYER_LOGIN");
 frame:SetScript("OnEvent", function(self, event, ...)
     -- Determines the player's region and loads that data
-    -- TODO Ideally we can load correct region on demand, rather than loading all and erasing what we don't need
     local region = GetCVar('portal');
-    if app["DATA_" .. region] then
+    local fn = app["Load" .. region];
+    if fn then
         print("Data for Azeroth: Loading data for " .. region);
-
-        app.CHARACTER_DATA = app["DATA_" .. region];
-
-        app.DATA_US = nil;
-        app.DATA_EU = nil;
-        app.DATA_TW = nil;
-        app.DATA_KR = nil;
+        fn();
     else
         -- TODO what should we do if the CVar doesn't work right?
         print("Data for Azeroth: Could not find player's region " .. region);
