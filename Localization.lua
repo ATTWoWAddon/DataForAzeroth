df10d20a26fc02e444dd2450443fd867cea83c40
shoogen <73682940+shoogen@users.noreply.github.com>
2025-08-07 00:09:33 -0400

    - Fixing indentation

--- Localization.lua
+++ Localization.lua
@@ -23,23 +23,23 @@ L.HidePercent_Tooltip = "Will hide the percent completed in the Data for Azeroth
 -- Localized Strings
 local locale = GetLocale():sub(3,4):lower();
 if locale == "es" then
-	L.RANK_FORMAT = "Mundo #%s  Región #%s  Reino #%s";
+    L.RANK_FORMAT = "Mundo #%s  Región #%s  Reino #%s";
 elseif locale == "mx" then
-	L.RANK_FORMAT = "Mundo #%s  Región #%s  Reino #%s";
+    L.RANK_FORMAT = "Mundo #%s  Región #%s  Reino #%s";
 elseif locale == "de" then
-	L.RANK_FORMAT = "Welt #%s  Region #%s  Server #%s";
+    L.RANK_FORMAT = "Welt #%s  Region #%s  Server #%s";
 elseif locale == "fr" then
-	L.RANK_FORMAT = "Monde #%s  Région #%s  Royaume #%s";
+    L.RANK_FORMAT = "Monde #%s  Région #%s  Royaume #%s";
 elseif locale == "it" then
-	L.RANK_FORMAT = "Mondo #%s  Regione #%s  Regno #%s";
+    L.RANK_FORMAT = "Mondo #%s  Regione #%s  Regno #%s";
 elseif locale == "pt" then
-	L.RANK_FORMAT = "Mundo #%s  Região #%s  Reino #%s";
+    L.RANK_FORMAT = "Mundo #%s  Região #%s  Reino #%s";
 elseif locale == "ru" then
-	L.RANK_FORMAT = "Мир #%s  Регион #%s  Сервер #%s";
+    L.RANK_FORMAT = "Мир #%s  Регион #%s  Сервер #%s";
 elseif locale == "ko" then
-	L.RANK_FORMAT = "월드 #%s  지역 #%s  영역 #%s";
+    L.RANK_FORMAT = "월드 #%s  지역 #%s  영역 #%s";
 elseif locale == "cn" then
-	L.RANK_FORMAT = "世界 #%s  地区 #%s  领域 #%s";
+    L.RANK_FORMAT = "世界 #%s  地区 #%s  领域 #%s";
 elseif locale == "tw" then
-	L.RANK_FORMAT = "世界 #%s  地區 #%s  領域 #%s";
+    L.RANK_FORMAT = "世界 #%s  地區 #%s  領域 #%s";
 end
--- Tooltips.lua
+++ Tooltips.lua
@@ -3,7 +3,7 @@ local L = app.L;
 
 -- Globals Cache
 local floor, max, min, pcall, select, tonumber, tostring
-	= floor, max, min, pcall, select, tonumber, tostring;
+    = floor, max, min, pcall, select, tonumber, tostring;
 
 -- Module locals
 -- This part was taken from AllTheThings with Crieve's permission
@@ -14,181 +14,176 @@ CS:Hide();
 -- Color AARRGGBB values used throughout ATT
 local colorStringFormat = "ff%02x%02x%02x";
 local function BindComponent(c)
-	-- Binds a color component value (0 and 1) to (0 and 255) instead.
-	return min(255, max(0, (c or 0) * 255));
+    -- Binds a color component value (0 and 1) to (0 and 255) instead.
+    return min(255, max(0, (c or 0) * 255));
 end
 local function HexToARGB(hex)
-	return tonumber("0x"..hex:sub(1,2)) / 255, tonumber("0x"..hex:sub(3,4)) / 255, tonumber("0x"..hex:sub(5,6)) / 255, tonumber("0x"..hex:sub(7,8)) / 255;
+    return tonumber("0x"..hex:sub(1,2)) / 255, tonumber("0x"..hex:sub(3,4)) / 255, tonumber("0x"..hex:sub(5,6)) / 255, tonumber("0x"..hex:sub(7,8)) / 255;
 end
 local function RGBToHex(r, g, b)
-	return colorStringFormat:format(
-		BindComponent(r),
-		BindComponent(g),
-		BindComponent(b));
+    return colorStringFormat:format(BindComponent(r), BindComponent(g), BindComponent(b));
 end
 local function RGBToHSV(r, g, b)
-  CS:SetColorRGB(r, g, b);
-  local h,s,v = CS:GetColorHSV()
-  return {h=h,s=s,v=v}
+    CS:SetColorRGB(r, g, b);
+    local h,s,v = CS:GetColorHSV()
+    return {h=h,s=s,v=v}
 end
 
 -- Generates a Color Scale between Red and Green with the Blue Completed color for 100%
 local colors, red, green, h = app.Colors, RGBToHSV(1,0,0), RGBToHSV(0,1,0);
 local ProgressColors = setmetatable({
-	[1] = "ff15abff",	-- ATT's 'Completed' Color
+    [1] = "ff15abff",    -- ATT's 'Completed' Color
 }, {
-	__index = function(t, p)
-		p = tonumber(p);
-		-- anything over 100% will just be 100% color
-		if p > 1 then return t[1]; end
-		-- anything somehow under 0 will just be 0
-		if p < 0 then return t[0]; end
-		if abs(red.h - green.h) > 180 then
-			local angle = (360 - abs(red.h - green.h)) * p;
-			if red.h < green.h then
-				h = floor(red.h - angle);
-				if h < 0 then h = 360 + h end
-			else
-				h = floor(red.h + angle);
-				if h > 360 then h = h - 360 end
-			end
-		else
-			h = floor(red.h-(red.h-green.h)*p)
-		end
-		CS:SetColorHSV(h, red.s-(red.s-green.s)*p, red.v-(red.v-green.v)*p);
-		local r,g,b = CS:GetColorRGB();
-		local color = RGBToHex(r, g, b);
-		t[p] = color;
-		return color;
-	end
+    __index = function(t, p)
+        p = tonumber(p);
+        -- anything over 100% will just be 100% color
+        if p > 1 then return t[1]; end
+        -- anything somehow under 0 will just be 0
+        if p < 0 then return t[0]; end
+        if abs(red.h - green.h) > 180 then
+            local angle = (360 - abs(red.h - green.h)) * p;
+            if red.h < green.h then
+                h = floor(red.h - angle);
+                if h < 0 then h = 360 + h end
+            else
+                h = floor(red.h + angle);
+                if h > 360 then h = h - 360 end
+            end
+        else
+            h = floor(red.h-(red.h-green.h)*p)
+        end
+        CS:SetColorHSV(h, red.s-(red.s-green.s)*p, red.v-(red.v-green.v)*p);
+        local r,g,b = CS:GetColorRGB();
+        local color = RGBToHex(r, g, b);
+        t[p] = color;
+        return color;
+    end
 });
 
 local function CanAttachTooltips()
-	return true;
+    return true;
 end
 local function ClearTooltip(self)
-	self.DataForAzerothProcessing = nil;
+    self.DataForAzerothProcessing = nil;
 end
 local function FormatNumericWithCommas(amount)
-  local k
-  while true do
-	amount, k = tostring(amount):gsub("^(-?%d+)(%d%d%d)", '%1,%2')
-	if k == 0 then
-		break
-	end
-  end
-  return amount
+    local k
+    while true do
+        amount, k = tostring(amount):gsub("^(-?%d+)(%d%d%d)", '%1,%2')
+        if k == 0 then break end
+    end
+    return amount
 end
 local function GetNumberWithZeros(number, desiredLength)
-	if desiredLength > 0 then
-		local str = tostring(number);
-		local length = str:len();
-		local pos = str:find("[.]");
-		if not pos then
-			str = str .. ".";
-			for i=desiredLength,1,-1 do
-				str = str .. "0";
-			end
-		else
-			local totalExtra = desiredLength - (length - pos);
-			for i=totalExtra,1,-1 do
-				str = str .. "0";
-			end
-			if totalExtra < 1 then
-				str = str:sub(1, pos + desiredLength);
-			end
-		end
-		return str;
-	else
-		return tostring(floor(number));
-	end
+    if desiredLength > 0 then
+        local str = tostring(number);
+        local length = str:len();
+        local pos = str:find("[.]");
+        if not pos then
+            str = str .. ".";
+            for i=desiredLength,1,-1 do
+                str = str .. "0";
+            end
+        else
+            local totalExtra = desiredLength - (length - pos);
+            for i=totalExtra,1,-1 do
+                str = str .. "0";
+            end
+            if totalExtra < 1 then
+                str = str:sub(1, pos + desiredLength);
+            end
+        end
+        return str;
+    else
+        return tostring(floor(number));
+    end
 end
 local function GetProgressColorText(progress, total)
-	if total and total > 0 then
-		local percent = min(1, (progress or 0) / total);
+    if total and total > 0 then
+        local percent = min(1, (progress or 0) / total);
 
-		local strmax, strpercent = "", "";
-		if not DFA_SETTINGS.HideMax then strmax = " / " .. FormatNumericWithCommas(total) end
-		if not DFA_SETTINGS.HidePercent then strpercent = " (" .. GetNumberWithZeros(percent * 100, 2) .. "%)" end
+        local strmax, strpercent = "", "";
+        if not DFA_SETTINGS.HideMax then strmax = " / " .. FormatNumericWithCommas(total) end
+        if not DFA_SETTINGS.HidePercent then strpercent = " (" .. GetNumberWithZeros(percent * 100, 2) .. "%)" end
 
-		return "|c" .. ProgressColors[percent] .. FormatNumericWithCommas(progress) .. strmax .. strpercent .. "|r";
-	end
+        return "|c" .. ProgressColors[percent] .. FormatNumericWithCommas(progress) .. strmax .. strpercent .. "|r";
+    end
 end
 local function GetRankColorText(rank)
-	if rank then
-		-- This is where you would determine a color based on the rank? (Maybe legendary for Top 10, Purple for Top 250, etc)
-		return FormatNumericWithCommas(rank);
-	end
-	return "??";
+    if rank then
+        -- This is where you would determine a color based on the rank? (Maybe legendary for Top 10, Purple for Top 250, etc)
+        return FormatNumericWithCommas(rank);
+    end
+    return "??";
 end
 local function AttachCharacterData(self, guid)
-	if guid then
-		-- Uncomment for debugging
-		-- self:AddDoubleLine("GUID", guid);
-		guid = app.ALTS[guid] or guid; -- lookup main character's GUID, if it exists
-		local data = app.CHARACTER_DATA[guid:gsub("Player%-", "")];
-		if data then
-			-- we have data, so add it to the tooltip
-			self:AddLine(" ");
-			self:AddLine(L.DATAFORAZEROTH);
-			if not DFA_SETTINGS.HideScore then
-				self:AddLine(GetProgressColorText(data[1],app.MAX_SCORE));
-			end
-			if not DFA_SETTINGS.HideRank then
-				self:AddLine(L.RANK_FORMAT:format(GetRankColorText(data[2]), GetRankColorText(data[3]), GetRankColorText(data[5])));
-			end
-		elseif app.ALTS[guid] == nil then
-			-- the target could be on an alt, whisper them ONCE to get their GUID
-			-- won't get a response if they don't have the addon or are hiding alts
-			local _, _, _, _, _, name, realm = GetPlayerInfoByGUID(guid);
-			if name and name ~= "" then
-				if realm and realm ~= "" then name = name .. "-" .. realm end
+    if guid then
+        -- Uncomment for debugging
+        -- self:AddDoubleLine("GUID", guid);
+        guid = app.ALTS[guid] or guid; -- lookup main character's GUID, if it exists
+        local data = app.CHARACTER_DATA[guid:gsub("Player%-", "")];
+        if data then
+            -- we have data, so add it to the tooltip
+            self:AddLine(" ");
+            self:AddLine(L.DATAFORAZEROTH);
+            if not DFA_SETTINGS.HideScore then
+                self:AddLine(GetProgressColorText(data[1],app.MAX_SCORE));
+            end
+            if not DFA_SETTINGS.HideRank then
+                self:AddLine(L.RANK_FORMAT:format(GetRankColorText(data[2]), GetRankColorText(data[3]), GetRankColorText(data[5])));
+            end
+        elseif app.ALTS[guid] == nil then
+            -- the target could be on an alt, whisper them ONCE to get their GUID
+            -- won't get a response if they don't have the addon or are hiding alts
+            local _, _, _, _, _, name, realm = GetPlayerInfoByGUID(guid);
+            if name and name ~= "" then
+                if realm and realm ~= "" then name = name .. "-" .. realm end
 
-				app.ALTS[guid] = false; -- prevents future whispers
-				--C_ChatInfo.SendAddonMessage("DFA", "WHO", "WHISPER", name); -- broken in 11.2?
-			end
-		end
-	end
+                app.ALTS[guid] = false; -- prevents future whispers
+                --C_ChatInfo.SendAddonMessage("DFA", "WHO", "WHISPER", name); -- broken in 11.2?
+            end
+        end
+    end
 end
 
 -- Tooltip API Differences between Modern and Legacy APIs.
 if TooltipDataProcessor and select(4, GetBuildInfo()) > 100000 then
-	-- 10.0.2
-	-- https://wowpedia.fandom.com/wiki/Patch_10.0.2/API_changes#Tooltip_Changes
-	local TooltipUtil = TooltipUtil;
-	local function AttachTooltip(self, ttdata)
-		if CanAttachTooltips() and not self.DataForAzerothProcessing then
-			self.DataForAzerothProcessing = true;
-			AttachCharacterData(self, select(3, TooltipUtil.GetDisplayedUnit(self)));
-			
-			-- Ensure that the processing state gets removed once the tooltip is cleared
-			if not self.DataForAzerothOnTooltipClearedHook then
-				pcall(self.HookScript, self, "OnTooltipCleared", ClearTooltip)
-				self.DataForAzerothOnTooltipClearedHook = true;
-			end
-		end
-	end
-	
-	TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit, AttachTooltip);
+    -- 10.0.2
+    -- https://wowpedia.fandom.com/wiki/Patch_10.0.2/API_changes#Tooltip_Changes
+    local TooltipUtil = TooltipUtil;
+    local function AttachTooltip(self, ttdata)
+        if CanAttachTooltips() and not self.DataForAzerothProcessing then
+            self.DataForAzerothProcessing = true;
+            AttachCharacterData(self, select(3, TooltipUtil.GetDisplayedUnit(self)));
+            
+            -- Ensure that the processing state gets removed once the tooltip is cleared
+            if not self.DataForAzerothOnTooltipClearedHook then
+                pcall(self.HookScript, self, "OnTooltipCleared", ClearTooltip)
+                self.DataForAzerothOnTooltipClearedHook = true;
+            end
+        end
+    end
+    
+    TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit, AttachTooltip);
 else
-	-- Pre-10.0.2 (Legacy)
-	local function AttachTooltip(self)
-		if CanAttachTooltips() and not self.DataForAzerothProcessing then
-			self.DataForAzerothProcessing = true;
+    -- Pre-10.0.2 (Legacy)
+    local function AttachTooltip(self)
+        if CanAttachTooltips() and not self.DataForAzerothProcessing then
+            self.DataForAzerothProcessing = true;
 
-			-- Does the tooltip have a target?
-			local target = select(2, self:GetUnit());
-			if target then AttachCharacterData(self, UnitGUID(target)); end
-			
-			-- Ensure that the processing state gets removed once the tooltip is cleared
-			if not self.DataForAzerothOnTooltipClearedHook then
-				pcall(self.HookScript, self, "OnTooltipCleared", ClearTooltip)
-				self.DataForAzerothOnTooltipClearedHook = true;
-			end
-		end
-	end
-	GameTooltip:HookScript("OnTooltipSetUnit", AttachTooltip);
-	GameTooltip:HookScript("OnShow", AttachTooltip);
+            -- Does the tooltip have a target?
+            local target = select(2, self:GetUnit());
+            if target then AttachCharacterData(self, UnitGUID(target)); end
+
+            -- Ensure that the processing state gets removed once the tooltip is cleared
+            if not self.DataForAzerothOnTooltipClearedHook then
+                pcall(self.HookScript, self, "OnTooltipCleared", ClearTooltip)
+                self.DataForAzerothOnTooltipClearedHook = true;
+            end
+        end
+    end
+    GameTooltip:HookScript("OnTooltipSetUnit", AttachTooltip);
+    GameTooltip:HookScript("OnShow", AttachTooltip);
 end
 
 -- Adds custom tooltip lines to each guild roster member in the Communities Frame
